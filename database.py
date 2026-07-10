46866aff59529aecb2897df1f44553ac833ffb70
Bruno Sena <168649781+brunosssn@users.noreply.github.com>
2026-07-10 15:15:57 -0300

    mudança no database

--- database.py
+++ database.py
@@ -1,65 +1,49 @@
 import sqlite3
+from threading import Lock
 
-DB_PATH = "bar.db"
+class DatabaseConfig:
+    _instance = None
+    _lock = Lock()
 
-def get_conn() -> sqlite3.Connection:
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row
-    conn.execute("PRAGMA foreign_keys = ON")
-    return conn
+    def __new__(cls):
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super(DatabaseConfig, cls).__new__(cls)
+                cls._instance.db_path = "bar.db"
+        return cls._instance
 
-def init_db():
-    with get_conn() as conn:
-        conn.executescript("""
-            CREATE TABLE IF NOT EXISTS funcionario (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                email       TEXT NOT NULL,
-                nome        TEXT NOT NULL,
-                cpf         VARCHAR(11) NOT NULL,
-                data_nascimento         DATE NOT NULL,
-                cargo       TEXT NOT NULL,
-                senha       TEXT NOT NULL
-            ); 
-            
-            CREATE TABLE IF NOT EXISTS gerente (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                email       TEXT NOT NULL,
-                nome        TEXT NOT NULL,
-                cpf         VARCHAR(11) NOT NULL,
-                data_nascimento         DATE NOT NULL,
-                senha       TEXT NOT NULL
-            );
-            
-            CREATE TABLE IF NOT EXISTS categoria (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                categoria       TEXT NOT NULL           
-            ); 
+    # Factory Method para fabricar e configurar conexões
+    def get_conn(self) -> sqlite3.Connection:
+        conn = sqlite3.connect(self.db_path)
+        conn.row_factory = sqlite3.Row
+        conn.execute("PRAGMA foreign_keys = ON")
+        return conn
 
-            CREATE TABLE IF NOT EXISTS produto (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                nome            TEXT NOT NULL,
-                preco           DECIMAL(4,2),
-                descrição           TEXT NOT NULL,
-                categoria_id            INTEGER NOT NULL,
-                FOREIGN KEY (categoria_id)      REFERENCES categoria(id)
-            );         
-
+    def init_db(self):
+        with self.get_conn() as conn:
+            conn.executescript("""
+                CREATE TABLE IF NOT EXISTS funcionario (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, nome TEXT NOT NULL,
+                    cpf VARCHAR(11) NOT NULL, data_nascimento DATE NOT NULL, cargo TEXT NOT NULL, senha TEXT NOT NULL
+                ); 
+                CREATE TABLE IF NOT EXISTS gerente (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL, nome TEXT NOT NULL,
+                    cpf VARCHAR(11) NOT NULL, data_nascimento DATE NOT NULL, senha TEXT NOT NULL
+                );
+                CREATE TABLE IF NOT EXISTS categoria (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT NOT NULL           
+                ); 
+                CREATE TABLE IF NOT EXISTS produto (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, preco DECIMAL(4,2),
+                    descrição TEXT NOT NULL, categoria_id INTEGER NOT NULL, FOREIGN KEY (categoria_id) REFERENCES categoria(id)
+                );         
                 CREATE TABLE IF NOT EXISTS pedido (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                numero_da_mesa          INTEGER NOT NULL,
-                estado_pedido           TEXT NOT NULL,        
-                funcionario_id            INTEGER NOT NULL,
-                data_criacao    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (funcionario_id)          REFERENCES funcionario(id)
-            );            
-
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, numero_da_mesa INTEGER NOT NULL, estado_pedido TEXT NOT NULL,        
+                    funcionario_id INTEGER NOT NULL, data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (funcionario_id) REFERENCES funcionario(id)
+                );            
                 CREATE TABLE IF NOT EXISTS pedido_produto (
-                id          INTEGER PRIMARY KEY AUTOINCREMENT,
-                quantidade          INTEGER NOT NULL,
-                pedido_id           INTEGER NOT NULL,
-                produto_id          INTEGER NOT NULL,
-                FOREIGN KEY (pedido_id)          REFERENCES pedido(id),
-                FOREIGN KEY (produto_id)          REFERENCES produto(id)  
-            );
-
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, quantidade INTEGER NOT NULL, pedido_id INTEGER NOT NULL,
+                    produto_id INTEGER NOT NULL, FOREIGN KEY (pedido_id) REFERENCES pedido(id), FOREIGN KEY (produto_id) REFERENCES produto(id)  
+                );
             """)
